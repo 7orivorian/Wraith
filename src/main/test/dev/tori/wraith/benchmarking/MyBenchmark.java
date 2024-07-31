d39f915ceb840c46c091b77dd7bc7b7845d38be0
Tori <7orivorian+github@gmail.com>
2024-07-31 00:32:45 -0400

    Update MyBenchmark
    Signed-off-by: Tori <7orivorian+github@gmail.com>

--- src/main/test/dev/tori/wraith/benchmarking/MyBenchmark.java
+++ src/main/test/dev/tori/wraith/benchmarking/MyBenchmark.java
@@ -24,7 +24,6 @@ package dev.tori.wraith.benchmarking;
 import dev.tori.wraith.bus.EventBus;
 import dev.tori.wraith.event.Target;
 import dev.tori.wraith.listener.EventListener;
-import dev.tori.wraith.subscriber.Subscriber;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
@@ -33,80 +32,33 @@ import java.util.concurrent.TimeUnit;
  * @author <a href="https://github.com/7orivorian">7orivorian</a>
  * @since 3.2.0
  */
+@State(Scope.Thread)
 public class MyBenchmark {
 
     @State(Scope.Thread)
     public static class BenchmarkState {
-        MyListener myListener;
-        MySubscriber mySubscriber;
+        EventListener<Object> myListener;
         EventBus bus;
 
         @Setup(Level.Trial)
         public void setup() {
             bus = new EventBus();
-            mySubscriber = new MySubscriber();
-            myListener = new MyListener();
+            myListener = new EventListener<>(Target.fine(Object.class)) {
+                @Override
+                public void invoke(Object event) {
+
+                }
+            };
         }
     }
 
     @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
+    @BenchmarkMode(Mode.SingleShotTime)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Warmup(iterations = 1, time = 2)
-    @Measurement(iterations = 2, time = 2)
+    @Warmup(iterations = 10)
+    @Measurement(iterations = 100)
     @Fork(5)
     public void testMethod(BenchmarkState state) {
-        for (int i = 0; i < 1_000; i++) {
-            state.bus.register(state.myListener);
-        }
-    }
-
-    public static class MySubscriber extends Subscriber {
-
-        public MySubscriber() {
-            registerListener(
-                    new MyListener()
-            );
-        }
-    }
-
-    public static class MyListener extends EventListener<MyEvent> {
-
-        public MyListener() {
-            super(Target.fine(MyEvent.class));
-        }
-
-        @Override
-        public void invoke(MyEvent event) {
-            System.out.println(event.content());
-        }
-
-        @Override
-        public boolean shouldPersist() {
-            return false;
-        }
-    }
-
-    public static final class MyEvent {
-
-        private String content;
-
-        public MyEvent(String content) {
-            this.content = content;
-        }
-
-        public String content() {
-            return content;
-        }
-
-        public void setContent(String content) {
-            this.content = content;
-        }
-
-        @Override
-        public String toString() {
-            return "MyEvent[" +
-                    "content=" + content + ']';
-        }
+        state.bus.register(state.myListener);
     }
 }
